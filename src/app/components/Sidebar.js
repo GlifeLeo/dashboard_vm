de67c7e7212e1585790ec63f937f4f0c1a07ef0e
Leo <leo.pham@glife.com.sg>
2024-11-09 12:14:21 +0700

    feat(layout): add layout and side bar [wip]

---
+++ src/app/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div
+      // style="background-color:'red'; color: "white" "
+      // style={{
+      //   backgroundColor: "green",
+      //   color: "white"
+      // }}
+      className='w-60 bg-green-500'>Sidebar</div>
+  )
+}
+
+export default Sidebar
